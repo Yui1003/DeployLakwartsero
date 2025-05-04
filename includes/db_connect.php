b2af03fe8a145d2a2b3308a154338907fb52edf4
Yui1003 <j.jomarie1435@gmail.com>
2025-05-04 11:43:28 +0800

    deployment stage

--- includes/db_connect.php
+++ includes/db_connect.php
@@ -1,9 +1,9 @@
 <?php
-// Local environment using XAMPP - MySQL connection
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "travel_db";
+// Database connection using environment variables
+$servername = getenv('DB_HOST');
+$username = getenv('DB_USER');
+$password = getenv('DB_PASS');
+$dbname = getenv('DB_NAME');
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
